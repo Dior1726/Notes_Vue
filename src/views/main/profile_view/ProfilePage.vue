28e6ba98c07a29d1a3928a1a021782ecb6ff395f
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2025-05-12 11:46:54 +0500

    feat: create Profile card component

--- src/views/main/profile_view/ProfilePage.vue
+++ src/views/main/profile_view/ProfilePage.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>ProfilePage</div>
+  <div v-if="isLoading" class="flex justify-center items-center">Loading...</div>
+  <div v-if="data">
+    <div className="text-xl mb-5 font-semibold">My Profile</div>
+    <profile-card :profile="data" />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query'
+import { getProfile } from './api/get_profile'
+import ProfileCard from './components/profile_card/ProfileCard.vue'
+
+const { data, isLoading } = useQuery({
+  queryKey: ['get_profile'],
+  queryFn: () => getProfile(),
+})
+</script>
---
+++ src/views/main/profile_view/components/profile_card/ProfileCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="flex flex-col gap-5">
+    <div class="flex items-center gap-10 bg-white shadow-sm rounded-lg p-4">
+      <div class="border border-gray-200 rounded-full overflow-hidden w-28 h-28">
+        <img class="w-full h-auto" :src="profile.image" :alt="profile.firstName" />
+      </div>
+      <div>
+        <h3 class="text-lg font-medium mb-4">{{ profile.firstName }} {{ profile.lastName }}</h3>
+        <p class="">{{ profile.email }}</p>
+        <p class="">{{ profile.company.title }}</p>
+      </div>
+    </div>
+    <div class="flex flex-col gap-5 bg-white shadow-sm rounded-lg p-4">
+      <div>
+        <h3>Personal information</h3>
+      </div>
+      <div class="flex">
+        <div class="flex-1">
+          <p class="text-slate-500 text-sm">First name</p>
+          <p>{{ profile.firstName }}</p>
+        </div>
+        <div class="flex-1">
+          <p class="text-slate-500 text-sm">Last name</p>
+          <p>{{ profile.lastName }}</p>
+        </div>
+      </div>
+      <div class="flex">
+        <div class="flex-1">
+          <p class="text-slate-500 text-sm">Email address</p>
+          <p>{{ profile.email }}</p>
+        </div>
+        <div class="flex-1">
+          <p class="text-slate-500 text-sm">Phone</p>
+          <p>{{ profile.phone }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col gap-5 bg-white shadow-sm rounded-lg p-4">
+      <div>
+        <h3>Address</h3>
+      </div>
+      <div class="flex">
+        <div class="flex-1">
+          <p class="text-slate-500 text-sm">Country</p>
+          <p>{{ profile.address.country }}</p>
+        </div>
+        <div class="flex-1">
+          <p class="text-slate-500 text-sm">State/City</p>
+          <p>{{ profile.address.state }}</p>
+        </div>
+      </div>
+      <div class="flex">
+        <div class="flex-1">
+          <p class="text-slate-500 text-sm">Address</p>
+          <p>{{ profile.address.address }}</p>
+        </div>
+        <div class="flex-1">
+          <p class="text-slate-500 text-sm">Postal code</p>
+          <p>{{ profile.address.postalCode }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps(['profile'])
+</script>
